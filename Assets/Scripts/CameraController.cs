8d6cd9e682b79f347d2dd1c05a60fc7c65d8095e
aaronthurston27 <41075873+aaronthurston27@users.noreply.github.com>
2019-07-18 21:54:29 -0400

    Added camera controller script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region const variables
+
+    #endregion
+
+    #region main variables
+
+    [SerializeField]
+    private Camera MainCamera;
+
+    [SerializeField]
+    public CustomBoxCollider2D LeftCameraBound;
+
+    [SerializeField]
+    public CustomBoxCollider2D RightCameraBound;
+
+    #endregion
+
+    #region monobehaviour methods
+
+    private void Awake()
+    {
+        if (MainCamera == null)
+        {
+            MainCamera = GetComponent<Camera>();
+        }
+    }
+
+    private void Update()
+    {
+        UpdateCameraPosition();
+    }
+
+    #endregion
+
+    #region private methods
+
+    private void UpdateCameraPosition()
+    {
+        if (MainCamera != null)
+        {
+
+        }
+    }
+
+    #endregion
+}
