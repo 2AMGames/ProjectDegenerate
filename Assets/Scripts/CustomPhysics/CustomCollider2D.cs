d08d38cff1cb77d5f2fd30a327e8198f3d3a16d4
Ryan Andersen <ryan.andersen05@gmail.com>
2019-07-21 20:39:24 -0400

    Adding some abstract methods for custom collider2D

--- Assets/Scripts/CustomPhysics/CustomCollider2D.cs
+++ Assets/Scripts/CustomPhysics/CustomCollider2D.cs
@@ -15,17 +15,18 @@ public abstract class CustomCollider2D : MonoBehaviour {
     public int verticalRayCount;
     public int horizontalRayCount;
 
-
     public CustomPhysics2D rigid { get; set; }
     public bool isStatic;
+
     /// <summary>
     /// 
     /// </summary>
     public ColliderBounds bounds { get; set; }
+
     /// <summary>
     /// 
     /// </summary>
-    protected ColliderBounds previouBounds { get; set; }
+    protected ColliderBounds previousBounds { get; set; }
 
     protected virtual void Awake()
     {
@@ -37,6 +38,7 @@ public abstract class CustomCollider2D : MonoBehaviour {
         }
         Overseer.Instance.ColliderManager.AddColliderToManager(this);
     }
+
 
     protected virtual void OnDestroy()
     {
@@ -64,7 +66,7 @@ public abstract class CustomCollider2D : MonoBehaviour {
     /// </summary>
     public virtual void UpdateBoundsOfCollider()
     {
-        previouBounds = bounds;
+        previousBounds = bounds;
     }
 
     /// <summary>
@@ -76,134 +78,44 @@ public abstract class CustomCollider2D : MonoBehaviour {
     /// <returns></returns>
     public abstract bool LineIntersectWithCollider(Vector2 origin, Vector2 direction, float length);
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    protected abstract bool CheckCollisionDownFromVelocity();
 
-    private bool UpdateCollisionUp()
-    {
-        if (rigid.velocity.y <= 0)
-        {
-            return false;
-        }
-        Vector2 adjustedPoint1 = bounds.topLeft + Vector2.right * HorizontalBuffer - VerticalBuffer * Vector2.up;
-        Vector2 adjustedPoint2 = bounds.topRight + Vector2.left * HorizontalBuffer - VerticalBuffer * Vector2.up;
-        CustomCollider2D[] tileCollidersThatWeHit = GetAllTilesHitFromRayCasts(adjustedPoint1, adjustedPoint2, Vector2.up,
-            Mathf.Abs(rigid.velocity.y * Time.deltaTime) + VerticalBuffer, verticalRayCount);
-        if (tileCollidersThatWeHit.Length == 0)
-        {
-            return false;
-        }
-        float lowestYValue = tileCollidersThatWeHit[0].bounds.bottomRight.y;
-        foreach (CustomCollider2D tile in tileCollidersThatWeHit)
-        {
-            Vector2 pointThatWeCollidedWith = new Vector2(this.transform.position.x, tile.bounds.bottomRight.y);
-            if (pointThatWeCollidedWith.y < lowestYValue)
-            {
-                lowestYValue = pointThatWeCollidedWith.y;
-            }
-        }
-        rigid.velocity.y = 0;
-        transform.position = new Vector3(transform.position.x, lowestYValue + (transform.position.y - bounds.topLeft.y), transform.position.z);
-        UpdateBoundsOfCollider();
-        return true;
-    }
 
-    private bool UpdateCollisionDown()
-    {
-        if (rigid.velocity.y > 0)
-        {
-            return false;
-        }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    protected abstract bool CheckCollisionUpFromVelocity();
 
-        Vector2 adjustedPoint1 = bounds.bottomLeft + Vector2.right * HorizontalBuffer + VerticalBuffer * Vector2.up;
-        Vector2 adjustedPoint2 = bounds.bottomRight + Vector2.left * HorizontalBuffer + VerticalBuffer * Vector2.up;
-        CustomCollider2D[] tileCollidersThatWeHit = GetAllTilesHitFromRayCasts(adjustedPoint1, adjustedPoint2, Vector2.down,
-            Mathf.Abs(rigid.velocity.y * Time.deltaTime) + VerticalBuffer, verticalRayCount);
-        if (tileCollidersThatWeHit.Length == 0)
-        {
-            return false;
-        }
-        float highestYValue = tileCollidersThatWeHit[0].bounds.topLeft.y;
-        foreach (CustomCollider2D tile in tileCollidersThatWeHit)
-        {
-            Vector2 pointThatWeCollidedWith = new Vector2(this.transform.position.x, tile.bounds.topLeft.y);
-            if (pointThatWeCollidedWith.y > highestYValue)
-            {
-                highestYValue = pointThatWeCollidedWith.y;
-            }
-        }
-        rigid.velocity.y = 0;
-        transform.position = new Vector3(transform.position.x, highestYValue + (transform.position.y - bounds.bottomLeft.y), transform.position.z);
-        UpdateBoundsOfCollider();//If we made it to the end, we wil need to update the collider bounds
-        return true;
-    }
 
-    private bool UpdateCollisionRight()
-    {
-        if (rigid.velocity.x <= 0)
-        {
-            return false;
-        }
-        Vector2 adjustedPoint1 = bounds.topRight + Vector2.right * HorizontalBuffer + VerticalBuffer * Vector2.up;
-        Vector2 adjustedPoint2 = bounds.bottomRight + Vector2.right * HorizontalBuffer + VerticalBuffer * Vector2.up;
-        CustomCollider2D[] tileCollidersThatWeHit = GetAllTilesHitFromRayCasts(
-            adjustedPoint1, adjustedPoint2, Vector2.right,
-            Mathf.Abs(rigid.velocity.x * Time.deltaTime) + HorizontalBuffer, horizontalRayCount);
-        if (tileCollidersThatWeHit.Length == 0)
-        {
-            return false;
-        }
-        //print(tileCollidersThatWeHit[0].name);
-        float lowestXValue = tileCollidersThatWeHit[0].bounds.bottomLeft.x;
-        foreach (CustomCollider2D tile in tileCollidersThatWeHit)
-        {
-            Vector2 pointThatWeCollidedWith = new Vector2(tile.bounds.bottomLeft.x, this.transform.position.y);
-            if (pointThatWeCollidedWith.x < lowestXValue)
-            {
-                lowestXValue = pointThatWeCollidedWith.x;
-            }
-        }
-        rigid.velocity.x = 0;
-        transform.position = new Vector3(lowestXValue + (transform.position.x - bounds.topRight.x), transform.position.y, transform.position.z);
-        UpdateBoundsOfCollider();
+    /// <summary>
+    /// Updates to see if we collided with any object to the Right
+    /// 
+    /// NOTE: Move this to CustomBoxCollider2D
+    /// </summary>
+    /// <returns></returns>
+    protected abstract bool CheckCollisionRightFromVelocity();
+    
 
-        return true;
-    }
+    /// <summary>
+    /// Updates to check if we have collided with an object to the left
+    /// 
+    /// NOTE: Move this over to CustomBoxCollider2D
+    /// </summary>
+    /// <returns></returns>
+    protected abstract bool CheckCollisionLeftFromVelocity();
+    
 
-    private bool UpdateCollisionLeft()
-    {
-        if (rigid.velocity.x >= 0)
-        {
-            return false;
-        }
-        Vector2 adjustedPoint1 = bounds.topLeft + Vector2.right * HorizontalBuffer + VerticalBuffer * Vector2.up;
-        Vector2 adjustedPoint2 = bounds.bottomLeft + Vector2.right * HorizontalBuffer + VerticalBuffer * Vector2.up;
-        CustomCollider2D[] tileCollidersThatWeHit = GetAllTilesHitFromRayCasts(
-            adjustedPoint1, adjustedPoint2,
-            Vector2.left, Mathf.Abs(rigid.velocity.x * Time.deltaTime) + HorizontalBuffer, horizontalRayCount);
-        if (tileCollidersThatWeHit.Length == 0)
-        {
-            return false;
-        }
-        //print(tileCollidersThatWeHit[0].name);
-        float highestXValue = tileCollidersThatWeHit[0].bounds.bottomRight.x;
-        foreach (CustomCollider2D tile in tileCollidersThatWeHit)
-        {
-            Vector2 pointThatWeCollidedWith = new Vector2(tile.bounds.bottomRight.x, this.transform.position.y);
-            if (pointThatWeCollidedWith.x > highestXValue)
-            {
-                highestXValue = pointThatWeCollidedWith.x;
-            }
-        }
-        rigid.velocity.x = 0;
-        transform.position = new Vector3(highestXValue + (transform.position.x - bounds.topLeft.x), transform.position.y, transform.position.z);
-        UpdateBoundsOfCollider();
-        return true;
-
-    }
-
+    /// <summary>
+    /// 
+    /// </summary>
     public virtual void CheckForCollisions()
     {
-        
-        if (UpdateCollisionDown())
+        if (CheckCollisionUpFromVelocity())
         {
             if (rigid.isInAir)
             {
@@ -219,10 +131,12 @@ public abstract class CustomCollider2D : MonoBehaviour {
                 rigid.OnPhysicsObjectAirborne();
             }
         }
-        UpdateCollisionUp();
-        UpdateCollisionLeft();
-        UpdateCollisionRight();
+        CheckCollisionDownFromVelocity();
+        CheckCollisionLeftFromVelocity();
+        CheckCollisionRightFromVelocity();
     }
+
+    public abstract void PushObjectOutsideOfCollider(CustomCollider2D collider);
 
     public abstract CustomCollider2D[] GetAllTilesHitFromRayCasts(Vector2 v1, Vector2 v2, Vector2 direction, float distance, int rayCount);
 
