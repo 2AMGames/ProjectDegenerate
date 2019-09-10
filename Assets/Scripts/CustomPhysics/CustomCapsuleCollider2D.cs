5a22c8a08952d55139f6dac9523e1719cd57bc75
Ryan Andersen <ryan.andersen05@gmail.com>
2019-09-10 11:05:00 -0400

    capsule collider should now update its bounds correctly

--- Assets/Scripts/CustomPhysics/CustomCapsuleCollider2D.cs
+++ Assets/Scripts/CustomPhysics/CustomCapsuleCollider2D.cs
@@ -11,6 +11,7 @@ public class CustomCapsuleCollider2D : CustomCollider2D
     public Vector2 capsuleOffset;
 
     public BoundsCapsule bounds;
+    public BoundsCapsule previousBounds;
 
     #region monobehaviour methods
     private void OnDrawGizmos()
@@ -60,24 +61,33 @@ public class CustomCapsuleCollider2D : CustomCollider2D
 
     
     /// <summary>
-    /// Updates the bounds of the capsule collider
+    /// Updates the bounds of the capsule collider. The Capsule collider is made up of one rect bounds and two circle bounds
     /// </summary>
     public override void UpdateBoundsOfCollider()
     {
-        //previousBounds = bounds;
+        previousBounds = bounds;
 
-        //BoundsRect b = new BoundsRect();
-        //Vector2 origin = this.transform.position + new Vector3(capsuleOffset.x, capsuleOffset.y);
-        //float xSize = drawHorizontal ? size : radius * 2;
-        //float ySize = drawHorizontal ? radius * 2 : size;
+        BoundsRect b = new BoundsRect();
+        Vector2 origin = this.transform.position + new Vector3(capsuleOffset.x, capsuleOffset.y);
+        float xSize = drawHorizontal ? size : radius * 2;
+        float ySize = drawHorizontal ? radius * 2 : size;
 
 
-        //b.topLeft = origin + Vector2.up * ySize / 2 - Vector2.right * xSize / 2;
-        //b.topRight = origin + Vector2.up * ySize / 2 + Vector2.right * xSize / 2;
-        //b.bottomLeft = origin - Vector2.up * ySize / 2 - Vector2.right * xSize / 2;
-        //b.bottomRight = origin - Vector2.up * ySize / 2 + Vector2.right * xSize / 2;
+        b.topLeft = origin + Vector2.up * ySize / 2 - Vector2.right * xSize / 2;
+        b.topRight = origin + Vector2.up * ySize / 2 + Vector2.right * xSize / 2;
+        b.bottomLeft = origin - Vector2.up * ySize / 2 - Vector2.right * xSize / 2;
+        b.bottomRight = origin - Vector2.up * ySize / 2 + Vector2.right * xSize / 2;
+        bounds.rectBounds = b;
 
-        //this.bounds = b;
+        BoundsCircle topCircle = new BoundsCircle();
+        topCircle.center = b.topLeft + (b.topRight - b.topLeft) / 2f;
+        topCircle.radius = radius;
+        bounds.topCircleBounds = topCircle;
+
+        BoundsCircle bottomCircle = new BoundsCircle();
+        bottomCircle.center = b.bottomLeft + (b.bottomRight - b.bottomLeft) / 2f;
+        bottomCircle.radius = radius;
+        bounds.bottomCircleBounds = bottomCircle;
     }
 
     public override Vector2 GetLowerBoundsAtXValue(float x)
