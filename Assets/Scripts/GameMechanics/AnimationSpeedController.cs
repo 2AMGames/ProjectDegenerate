126b93e8e1ffe812d3de248ed8f59d360b5aac72
aaronthurston27 <41075873+aaronthurston27@users.noreply.github.com>
2019-09-15 21:20:28 -0400

    Added animation speed controller
    Controller scales animator speed depending on time of previous frame. This way, we can speed up or slow down animations to keep a consistent duration (due to network lag).

---
+++ Assets/Scripts/GameMechanics/AnimationSpeedController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSpeedController : MonoBehaviour
+{
+    #region main variables
+
+    private Animator Animator;
+
+    #endregion
+
+    #region monobehaviour methods
+
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        SetAnimationSpeed();
+    }
+
+    #endregion
+
+    #region private methods
+
+    private void SetAnimationSpeed()
+    {
+        float speedModifier = Overseer.DELTA_TIME > 0 ? Time.deltaTime / Overseer.DELTA_TIME : 0f;
+        Animator.speed = speedModifier;
+    }
+
+    #endregion
+}
