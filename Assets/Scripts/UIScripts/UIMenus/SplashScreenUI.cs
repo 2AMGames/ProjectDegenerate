ba9156f04f0744d4b601eba05c405a18868f23f0
Ryan Andersen <ryan.andersen05@gmail.com>
2019-10-26 17:29:21 -0400

    Splash screen now goes straight to game

--- Assets/Scripts/UIScripts/UIMenus/SplashScreenUI.cs
+++ Assets/Scripts/UIScripts/UIMenus/SplashScreenUI.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Utilities;
+using UnityEngine.UI;
 
 /// <summary>
 /// This will be the main start up screen. This is not really a main menu for the game. It is more to confirm who is the main player
 /// 
 /// Whoever presses start here should most likely be set to player 1 by default. We can change that later.
 /// </summary>
-public class SplashScreenUI : BaseSelectionUI
+public class SplashScreenUI : MonoBehaviour
 {
+    private const float TIME_TO_FADE_SCENE = .5f;
+    public Image fadeToBlackImage;
     public SceneField mainMenuSceneToLoad;
 
     #region monobehaviour methods
@@ -21,15 +25,49 @@ public class SplashScreenUI : BaseSelectionUI
 
     private void Update()
     {
-        
+
+        if (GetStartButtonPressed())
+        {
+            StartCoroutine(LoadNewScene());
+        }
     }
     #endregion monobehaviour methods
 
 
     #region button input events
-    public void OnStartButtonPressed()
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private bool GetStartButtonPressed()
+    {
+        return Input.GetButtonDown(BaseSelectionUI.ACCEPT_BUTTON);
+    }
+
+    private void OnStartButtonPressed()
     {
 
     }
     #endregion button input events
+
+    private IEnumerator LoadNewScene()
+    {
+
+
+        float timeThatHasPassed = 0;
+        AsyncOperation asynLoad = SceneManager.LoadSceneAsync(mainMenuSceneToLoad.SceneName);
+        asynLoad.allowSceneActivation = false;
+
+        Color originalColor = fadeToBlackImage.color;
+        Color goalColor = new Vector4(0, 0, 0, 1);
+        while (timeThatHasPassed < TIME_TO_FADE_SCENE)
+        {
+            timeThatHasPassed += Time.unscaledDeltaTime;
+            fadeToBlackImage.color = Vector4.Lerp(originalColor, goalColor, timeThatHasPassed / TIME_TO_FADE_SCENE);
+            yield return null;
+        }
+        fadeToBlackImage.color = goalColor;
+        asynLoad.allowSceneActivation = true;
+    }
+
 }
