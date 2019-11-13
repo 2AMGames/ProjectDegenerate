b805b810d916d6acba322ca680390e0f9b0d8d77
Ryan Andersen <ryan.andersen05@gmail.com>
2019-11-13 00:23:02 -0500

    Adding a script that specifically handles Yukari's mechanics

---
+++ Assets/Scripts/CharacterHandlers/YukariMechanics.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// This class is used to carry out specific functions and mechanics that would only apply to the Character 'Yukari'
+/// </summary>
+public class YukariMechanics : MonoBehaviour
+{
+    [Tooltip("A prefab reference to Yukaris Arrow projectile")]
+    public YukariArrow yukariArrowPrefab;
+
+    #region monobehaivour methods
+    private void Awake()
+    {
+        SpawnPool.Instance.InitializeSpawnPool(yukariArrowPrefab, 3);//Initialize spawn pool so that we can appropriately use Yukari's arrows
+    }
+    #endregion monobehaviour methods
+
+    #region event methods
+
+
+    #endregion event methods
+}
