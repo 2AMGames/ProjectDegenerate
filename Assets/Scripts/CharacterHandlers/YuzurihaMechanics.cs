d20bf9a9c8b085452a0241a1be9f2ac8eeef856b
aaronthurston27 <41075873+aaronthurston27@users.noreply.github.com>
2019-12-27 22:08:21 -0500

    Started Yuzuriha mechanics

--- Assets/Scripts/CharacterHandlers/YuzurihaMechanics.cs
+++ Assets/Scripts/CharacterHandlers/YuzurihaMechanics.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class YuzurihaMechanics : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region fields
+
+    private bool LightAttack;
+
+    private bool MediumAttack;
+
+    private bool HeavyAttack;
+
+    private CommandInterpreter CommandInterpreter;
+
+    private Animator Animator;
+
+    #endregion
+
+    #region monobehaviour
+
+    private void Start()
     {
-        
+        CommandInterpreter = GetComponent<CommandInterpreter>();
+        Animator = GetComponent<Animator>();
+        ResetStanceAttacks();
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    #region public inteferface
+
+    public void ResetStanceAttacks()
     {
-        
+        LightAttack = true;
+        MediumAttack = true;
+        HeavyAttack = true;
+
+        UpdateAnimator();
     }
+
+    public void HighAttackStart()
+    {
+        LightAttack = false;
+        UpdateAnimator();
+    }
+
+    public void MidAttackStart()
+    {
+        MediumAttack = false;
+        UpdateAnimator();
+    }
+
+    public void LowAttackStart()
+    {
+        HeavyAttack = false;
+        UpdateAnimator();
+    }
+
+    #endregion
+
+    #region private interface
+
+    private void UpdateAnimator()
+    {
+        Animator.SetBool("LightReady", LightAttack);
+        Animator.SetBool("MediumReady", MediumAttack);
+        Animator.SetBool("HeavyReady", HeavyAttack);
+    }
+
+    #endregion
 }
