de3c71fd1b95975da106e59f61a7f820837ffe1b
aaronthurston27 <41075873+aaronthurston27@users.noreply.github.com>
2020-01-18 03:26:11 -0500

    Fixed photon server settings

--- Assets/Scripts/Overseer.cs
+++ Assets/Scripts/Overseer.cs
@@ -44,12 +44,14 @@ public class Overseer : MonoBehaviour, IOnEventCallback, IInRoomCallbacks
             {
                 instance = FindObjectOfType<Overseer>();
             }
+            /*
             if (instance == null)
             {
                 GameObject container = new GameObject("Overseer");
                 instance = container.AddComponent<Overseer>();
                 Debug.LogWarning("Make sure to assign GameOverseer to an instance of an object in the Hierarchy.");//if we got here, we should make sure to assign it ourselves
             }
+            */
             return instance;
         }
     }
