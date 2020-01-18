336aed4dd12a648e4c15139850ab71ee7613433f
Ryan Andersen <ryan.andersen05@gmail.com>
2020-01-17 22:59:14 -0500

    Adding Isis sprites

---
+++ Assets/Scripts/CharacterHandlers/IsisMechanics.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script handles all the mechanics related to Yukari's Persona Assistant, Isis.
+/// </summary>
+public class IsisMechanics : InteractionHandler
+{
+
+
+    #region monobehaviour methods
+    public override void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    #endregion monobehaviour methods
+    #region animator events
+    public void OnLaunchAirProjectile()
+    {
+
+    }
+    #endregion animator events
+}
